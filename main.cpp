4d85d54c834a4d91071a6e87cbd29f841318701b
Tomoki <tomokin@mtl.t.u-tokyo.ac.jp>
2019-01-02 12:47:33 +0900

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include "defines.hpp"
+
+int main(int argc, char* argv[]) {
+
+    /*ソケットを作成*/
+    int arp_sock = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_ARP));
+    if(arp_sock < 0) {
+        perror("Create arp sock");
+        return 1;
+    }
+
+    /*ARPのソケットをbind*/
+    struct sockaddr_ll sockaddr;
+    memset(&sockaddr, 0x0, sizeof(sockaddr));
+    sockaddr.sll_family = AF_PACKET;
+    sockaddr.sll_protocol = htons(ETH_P_ARP);
+    sockaddr.sll_ifindex = if_nametoindex("eth0");
+    if(bind(arp_sock, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
+        perror("Bind arp sock");
+        return 1;
+    }
+
+    /*Dash buttonのMACアドレスを格納*/
+    unsigned char button_addr[6];
+    read_button_address(button_addr);
+    
+    /*受信*/
+    while(1) {
+      char buf[256];
+      memset(buf,0x0,sizeof(buf));
+      int arp_size = recvfrom(arp_sock, buf, sizeof(buf), 0, NULL, NULL);
+      if(arp_size < 0) {
+         perror("Receive arp socket");
+      }
+
+      /*受信したデータをARPパケットにキャスト*/
+      struct ether_arp *arppack = (struct ether_arp*) buf;
+      if(addr_matching(button_addr,arppack->arp_sha)){
+          //cout << "match !" << endl;
+          execute();
+      }
+    }
+    return 0;
+}
+
+
